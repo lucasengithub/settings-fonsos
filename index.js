576d233fba6c07ce62be8fc897f78c1c644902b0
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2024-02-23 13:48:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const electron = require("electron");
 const { app, BrowserWindow } = electron;
+const { autoUpdater, AppUpdater } = require("electron-updater");
 
 
+
+//Update Flags
+
+
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
 
 const createWindow = () => {
     const configs = {
         width: 800,
         height: 600,
+        icon: __dirname + 'preferences-system.png',
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
+        
     };
+
+    autoUpdater.checkForUpdates();
 
     const window = new BrowserWindow(configs);
 
